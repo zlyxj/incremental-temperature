361e1b44e64d635288433849171edafe817aab04
zlyxj <xiaomei8229@sina.com>
2025-01-04 12:19:55 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
-document.getElementById("t1").setAttribute('display',block);
-document.getElementById("t2").setAttribute('display',none);
+document.getElementById("t1").className="normalTab"
+document.getElementById("t2").className="hiddenTab"
 var game={
   temp=new D(10),
   hotgens=[0,0,0,0,0,0,0,0]
 }
 function tab1(){
-document.getElementById("t1").setAttribute('display',block);
-document.getElementById("t2").setAttribute('display',none);
+document.getElementById("t1").className="normalTab"
+document.getElementById("t2").className="hiddenTab"
 }
 function tab2(){
-document.getElementById("t2").setAttribute('display',block);
-document.getElementById("t1").setAttribute('display',none);
+document.getElementById("t1").className="normalTab"
+document.getElementById("t2").className="hiddenTab"
 }
