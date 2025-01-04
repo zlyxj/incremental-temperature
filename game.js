b5a72f4d291c016e6f084feef96607d22fb1b2e3
zlyxj <xiaomei8229@sina.com>
2025-01-04 17:09:20 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 document.getElementById("t1").className="normalTab"
 document.getElementById("t2").className="hiddenTab"
 var game={
-  temp=new D(10),
-  hotgens=[0,0,0,0,0,0,0,0]
+  temp:new D(10),
+  hotgens:[0,0,0,0,0,0,0,0]
 }
 function tab1(){
 document.getElementById("t1").className="normalTab"
