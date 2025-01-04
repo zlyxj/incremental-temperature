31105fb8353103963db5841e8932396a68cd2d18
zlyxj <xiaomei8229@sina.com>
2025-01-04 10:11:26 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
+document.getElementById("t1").setAttribute('display',block);
+document.getElementById("t2").setAttribute('display',none);
 var game={
   temp=new D(10),
   hotgens=[0,0,0,0,0,0,0,0]
 }
 function tab1(){
-  document.getElementById("t1").display=block;
-  document.getElementById("t2").display=none;
+document.getElementById("t1").setAttribute('display',block);
+document.getElementById("t2").setAttribute('display',none);
 }
 function tab2(){
-  document.getElementById("t2").display=block;
-  document.getElementById("t1").display=none;
+document.getElementById("t2").setAttribute('display',block);
+document.getElementById("t1").setAttribute('display',none);
 }
