0d27e850860c3a1b65f6008c94a9f4622d2f9ffc
zlyxj <xiaomei8229@sina.com>
2025-01-04 10:03:19 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,12 @@
+var game={
+  temp=new D(10),
+  hotgens=[0,0,0,0,0,0,0,0]
+}
+function tab1(){
+  document.getElementById("t1").display=block;
+  document.getElementById("t2").display=none;
+}
+function tab2(){
+  document.getElementById("t2").display=block;
+  document.getElementById("t1").display=none;
+}
